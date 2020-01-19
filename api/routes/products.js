f49b3096422949de82fd28c650ad86f8aed6b008
axelkonefal <axelkonefal@gmail.com>
2020-01-19 23:42:09 +0100

    final

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+       cb(null, './uploads/');     
+    },
+    filename: function(req, file, cb){
+        cb(null, Date.now() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true); 
+    }else{
+        cb(null, false);
+    }
+
+    
+
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {
+    fileSize: 1024 * 1024 * 5
+},
+fileFilter: fileFilter
+});
 
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) =>{
     Product.find()
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(docs =>{
         const response = {
@@ -15,6 +45,7 @@ router.get('/', (req, res, next) =>{
                 return {
                     name: docs.name,
                     price: doc.price,
+                    productImage: doc.productImage,
                     _id: doc._id,
                     request: {
                         type: 'GET',
@@ -40,11 +71,12 @@ router.get('/', (req, res, next) =>{
     });
 });
 
-router.post('/', (req, res, next) =>{
+router.post('/',checkAuth ,upload.single('productImage'), (req, res, next) =>{
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        productImage: req.file.path
     });
     product
     .save()
@@ -75,7 +107,7 @@ router.post('/', (req, res, next) =>{
 router.get('/:productId', (req,res,next) =>{
     const id = req.params.productId;
     Product.findById(id)
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(doc =>{
         console.log("From database", doc);
@@ -101,7 +133,7 @@ router.get('/:productId', (req,res,next) =>{
     });
 });
 
-router.patch('/:productId', (req,res,next) =>{
+router.patch('/:productId',checkAuth ,(req,res,next) =>{
     const id = req.params.productId;
     const updateOps = {};
     for(const ops of req.body){
@@ -126,7 +158,7 @@ router.patch('/:productId', (req,res,next) =>{
     });
 });
 
-router.delete('/:productId', (req,res,next) =>{
+router.delete('/:productId',checkAuth, (req,res,next) =>{
     const id = req.params.productId;
     Product.remove({ _id: id })
     .exec()
